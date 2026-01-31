09cf39280fd7a051cc3f8a1369c577be4dea3839
Ivan Nikcevic <nikcevici13@gmail.com>
2026-01-31 15:11:36 +0100

    3d gradient, 3 points

--- main.py
+++ main.py
@@ -13,8 +13,36 @@ y = np.arange(-1, 1, 0.05)
 
 X,Y = np.meshgrid(x,y)
 Z = z_function(X,Y)
+current_pos = (0.7, 0.4, z_function(0.7, 0.4))
+current_pos1 = (-0.3, 0.1, z_function(0.7, 0.4))
+current_pos2 = (0.6, -0.7, z_function(0.7, 0.4))
 
-ax = plt.subplot(projection='3d')
-ax.plot_surface(X, Y, Z, cmap='viridis', alpha=0.8)
+learning_rate = 0.01
+
+ax = plt.subplot(projection='3d', computed_zorder =False)
+
+for _ in range(1000):
+  X_derivative, Y_derivative = calculate_gradient(current_pos[0], current_pos[1])
+  X_new, Y_new = current_pos[0] - learning_rate * X_derivative, current_pos[1] - learning_rate * Y_derivative
+  current_pos = (X_new, Y_new, z_function(X_new, Y_new))
+
+
+  X_derivative, Y_derivative = calculate_gradient(current_pos1[0], current_pos1[1])
+  X_new, Y_new = current_pos1[0] - learning_rate * X_derivative, current_pos1[1] - learning_rate * Y_derivative
+  current_pos1 = (X_new, Y_new, z_function(X_new, Y_new))
+
+  X_derivative, Y_derivative = calculate_gradient(current_pos2[0], current_pos2[1])
+  X_new, Y_new = current_pos2[0] - learning_rate * X_derivative, current_pos2[1] - learning_rate * Y_derivative
+  current_pos2 = (X_new, Y_new, z_function(X_new, Y_new))
+
+
+  ax.plot_surface(X,Y,Z, cmap = 'viridis', zorder = 0)
+  ax.scatter(current_pos[0], current_pos[1], current_pos[2], color='magenta', zorder = 1)
+  ax.scatter(current_pos1[0], current_pos1[1], current_pos1[2], color='cyan', zorder = 1)
+  ax.scatter(current_pos2[0], current_pos2[1], current_pos2[2], color='yellow', zorder = 1)
+  plt.pause(0.001)
+  ax.clear()
+
+
 plt.show()
 
